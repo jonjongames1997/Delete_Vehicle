fccc648198d92eb7e7ab8c5b857e2facce3c7754
Jonathan Morrison <87885521+jonjongames1997@users.noreply.github.com>
2025-12-24 22:55:01 -0500

    Update plugin metadata and bump version to 1.3.0
    Changed plugin name and author in AssemblyInfo.cs, and updated AssemblyVersion and AssemblyFileVersion to 1.3.0.

--- RPH_Delete_Vehicle/Properties/AssemblyInfo.cs
+++ RPH_Delete_Vehicle/Properties/AssemblyInfo.cs
@@ -5,8 +5,8 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: Plugin("RPH Delete Vehicle", Description = "Simple plugin to delete the closest vehicle", PrefersSingleInstance = true, Author = "Waynieoaks")]
-[assembly: AssemblyTitle("RPH Delete Vehicle")]
+[assembly: Plugin("RPH_Delete_Vehicle", Description = "Simple plugin to delete the closest vehicle", PrefersSingleInstance = true, Author = "Waynieoaks/JM Modifications")]
+[assembly: AssemblyTitle("RPH_Delete_Vehicle")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
@@ -33,5 +33,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.3")]
-[assembly: AssemblyFileVersion("1.2.3")]
+[assembly: AssemblyVersion("1.3.0")]
+[assembly: AssemblyFileVersion("1.3.0")]
